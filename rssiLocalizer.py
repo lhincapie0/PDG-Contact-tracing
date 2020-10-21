9361c63b2981abed3b54843179c03b078e7d663e
davidobando99 <da.vi.d99@hotmail.com>
2020-10-20 21:54:49 -0500

    calculate intersections

--- rssiLocalizer.py
+++ rssiLocalizer.py
@@ -1,7 +1,8 @@
 import psycopg2
+import math
 from rssi import RSSI_Localizer
 
-print("hola")
+EPSILON = 0.000001
 accessPoint = {
     'signalAttenuation': 3,
     'location': {
@@ -22,6 +23,82 @@ distance = rssi_localizer_instance.getDistanceFromAP(
     accessPoint, signalStrength)
 
 
+def calculate_intersections(x0, y0, r0, x1,  y1, r1):
+
+    intersections = []
+
+    distance_x = x1 - x0
+    distance_y = y1 - y0
+
+    d = math.sqrt((distance_y * distance_y) + (distance_x * distance_x))
+    if (d > (r0 + r1)):
+        return None
+    if (d < abs(r0 - r1)):
+        return None
+    a = ((r0 * r0) - (r1 * r1) + (d * d)) / (2.0 * d)
+
+    point2_x = x0 + (distance_x * a / d)
+    point2_y = y0 + (distance_y * a / d)
+
+    h = math.sqrt((r0 * r0) - (a * a))
+
+    rx = -distance_y * (h / d)
+    ry = distance_x * (h / d)
+
+    intersectionPoint1_x = point2_x + rx
+    intersectionPoint2_x = point2_x - rx
+    intersectionPoint1_y = point2_y + ry
+    intersectionPoint2_y = point2_y - ry
+
+    intersections.append(intersectionPoint1_x)
+    intersections.append(intersectionPoint2_x)
+    intersections.append(intersectionPoint1_y)
+    intersections.append(intersectionPoint2_y)
+    return intersections
+
+
+def trilateration(x0, y0, r0, x1,  y1, r1, x2, y2, r2):
+    dx = x1 - x0
+    dy = y1 - y0
+
+    intersectionsAP12 = calculate_intersections(x0, y0, r0, x1, y1, r1)
+    # Determine the absolute intersection points.
+    intersectionPoint1_x = intersectionsAP12[0]
+    intersectionPoint2_x = intersectionsAP12[1]
+    intersectionPoint1_y = intersectionsAP12[2]
+    intersectionPoint2_y = intersectionsAP12[3]
+
+    print("INTERSECTION AP1 AND AP2:" + "(" + str(intersectionPoint1_x) + "," + str(intersectionPoint1_y) + ")"
+          + " AND (" + str(intersectionPoint2_x) + "," + str(intersectionPoint2_y) + ")")
+    intersectionsAP13 = calculate_intersections(x0, y0, r0, x2, y2, r2)
+
+    print("INTERSECTION AP1 AND AP3:" + "(" + str(intersectionsAP13[0]) + "," + str(intersectionsAP13[2]) + ")"
+          + " AND (" + str(intersectionsAP13[1]) + "," + str(intersectionsAP13[3]) + ")")
+
+    intersectionsAP23 = calculate_intersections(x1, y1, r1, x2, y2, r2)
+
+    print("INTERSECTION AP2 AND AP3:" + "(" + str(intersectionsAP23[0]) + "," + str(intersectionsAP23[2]) + ")"
+          + " AND (" + str(intersectionsAP23[1]) + "," + str(intersectionsAP23[3]) + ")")
+
+    dx = intersectionPoint1_x - x2
+    dy = intersectionPoint1_y - y2
+    d1 = math.sqrt((dy * dy) + (dx * dx))
+
+    dx = intersectionPoint2_x - x2
+    dy = intersectionPoint2_y - y2
+    d2 = math.sqrt((dy * dy) + (dx * dx))
+
+    if (abs(d1 - r2) < EPSILON):
+        print("INTERSECTION AP1 AND AP2 AND AP3:" + "(" + intersectionPoint1_x + ","
+              + intersectionPoint1_y + ")")
+    elif (abs(d2 - r2) < EPSILON):
+        print("INTERSECTION AP1 AND AP2 AND AP3:" + "(" + intersectionPoint2_x + ","
+              + intersectionPoint2_y + ")")
+        # here was an error
+    else:
+        print("INTERSECTION AP1 AND AP2 AND AP3:" + " NONE")
+
+
 try:
     connection = psycopg2.connect(user="postgres",
                                   password="firewall990612",
@@ -36,16 +113,16 @@ try:
     aps_records = cursor.fetchall()
 
     print("Print each row and it's columns values")
-    for row in aps_records:
-        if(row[1] == 4 or row[1] == 3):
-            print("MAC = ", row[0], )
-            print("Observers = ", row[1])
-            print("Seen Time  = ", row[2], "\n")
-            print(row[3])
-            print(row[4])
-            print(row[5])
-            print(row[6])
 
+#  for row in aps_records:
+#         if(row[1] == 4 or row[1] == 3):
+#             print("MAC = ", row[0])
+#             print("Observers = ", row[1])
+#             print("Seen Time  = ", row[2], "\n")
+#             print(row[3])
+#             print(row[4])
+#             print(row[5])
+#             print(row[6])
 
 except (Exception, psycopg2.Error) as error:
     print("Error while fetching data from PostgreSQL", error)
@@ -57,3 +134,8 @@ finally:
         connection.close()
         print("PostgreSQL connection is closed")
 print(distance)
+
+# example with coordinates APinfo
+print(trilateration(12.92732259, -27.85846534, 3.0,
+                    11.32153227, -27.83017898, 2.0,
+                    4.149949553, -26.74243983, 8.0))
