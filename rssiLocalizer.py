389d7a99ea1e9b869ccb2c5f76ce7afa019ba6d7
lhincapie0 <lauhincapie97@gmail.com>
2020-10-26 21:12:34 -0500

    Get distances from aps

--- rssiLocalizer.py
+++ rssiLocalizer.py
@@ -39,31 +39,59 @@ def generateRandomLocation(ap):
     ed_e = ap.location.find("Edificio E")
     ed_d = ap.location.find("Edificio D")
     ed_sae = ap.location.find("Casa_SAE")
-
+    ed_bien = ap.location.find("Bienestar")
 
     if (ed_e > 0):
-        ap.x = np.random.randint(100, 200)
-        ap.y = np.random.randint(0, 100)
-    if (ed_d > 0):
-        ap.x = np.random.randint(300, 400)
-        ap.y = np.random.randint(100, 150)
-    if (ed_sae > 0):
-        x = np.random.randint(700, 800)
-        y = np.random.randint(300, 360)
+        x = np.random.rand() *100 +200
+        y = np.random.rand() *100 +200
         ap.x = x
         ap.y = y
+    if (ed_d > 0):
+        x = np.random.rand() *100 + 300
+        y = np.random.rand()  *100 + 300
+        ap.x = x
+        ap.y = y
+    if (ed_sae > 0):
+        x = np.random.rand()  *100 + 700
+        y = np.random.rand() *100 + 700
+        ap.x = x
+        ap.y = y
+    if (ed_bien > 0):
+        x = np.random.rand()  *100 + 400
+        y = np.random.rand() *100 + 400
+        ap.x = x
+        ap.y = y
+    
+    
     return ap
 
 def evaluateMatches():
     print("evaluating observers ... ")
+    i = 0
     for observer in observers:
         for accesspoint in accesspoints:
             if(accesspoint.mac == observer.ap_mac):
-                print("ttrue")
-                print(observer.ap_mac)
-                print(access_point.x)
-                print(access_point.y)
+                calculateDistance(observer, accesspoint)
 
+def calculateDistance(observer, accesspoint):
+    accessPoint = {
+    'signalAttenuation': 3,
+    'location': {
+        'y': accesspoint.y,
+        'x': accesspoint.x
+    },
+    'reference': {
+        'distance': 4,
+        'signal': -50
+    },
+        'name': accesspoint.host_name
+    }
+    rssi_localizer_instance = RSSI_Localizer(accessPoints=accessPoint)
+    signalStrength = observer.rssi
+    distance = rssi_localizer_instance.getDistanceFromAP(
+        accessPoint, int(observer.rssi))
+    print('Phone was seen at ' +observer.seentime + ' for AP: '+ observer.ap_mac + ' at: ') 
+    print(distance['distance'])
 
 connection = None
 try:
@@ -74,7 +102,7 @@ try:
     cursor.execute(postgreSQL_select_Query)
     allData = cursor.fetchall()
     seq = allData
-    #seq.sort(key = lambda x: x[0])
+
     groups = groupby(seq, lambda x: x[0])
     aps = [[item[:] for item in data] for (key, data) in groups]
     for element in aps:
@@ -82,7 +110,6 @@ try:
             access_point = access_point_profile(ap[1], ap[2], ap[6], ap[14])
             access_point = generateRandomLocation(access_point)
             accesspoints.append(access_point)
-            
 
     postgreSQL_select_Query = """SELECT res.mac, res.seenTime, spaces."MAC", res.rssi
         FROM(SELECT 
