97cc2633a1b3cce4b34c36cf8dffb3a7fe08ffd1
= <=>
2021-01-10 11:39:41 -0500

    Comments for VP and graphics scripts

--- Graphics/scriptG.py
+++ Graphics/scriptG.py
@@ -7,6 +7,10 @@ import datetime
 from psycopg2 import connect
 from psycopg2 import OperationalError, errorcodes, errors
 
+# El siguiete script tiene como función generar las gráficas de número de observadores presentados por los datos recogidos con el Webhook de Aerohive.
+
+# Metodo que define los errores y excepciones de impresión
+
 
 def print_psycopg2_exception(err):
     # get details about the exception
@@ -22,35 +26,40 @@ def print_psycopg2_exception(err):
     print("pgcode:", err.pgcode, "\n")
 
 
+# Variable que contiene la conexión a la base de datos de postgresql donde están cargados los archivos JSON con los datos de las pruebas
 connection = None
 try:
+    # Conexión a la base de datos. Esta corre en un servidor local de postgresql
     connection = psycopg2.connect(
         user="postgres", password="postgres", host="127.0.0.1", port="5432", database="pdgdata")
     cursor = connection.cursor()
-    postgreSQL_select_Query = "SELECT * FROM(SELECT json_array_elements(data->'data'->'observations')->>'clientMac' as mac,json_array_elements(data->'data'->'observations')->>'seenTime' as time,json_array_length(json_array_elements(data->'data'->'observations')->'deviceObservers') as observers FROM public.example2dic ORDER BY mac,time) as res WHERE res.mac='F81F32F89FB4';"
+    # Consulta que obtiene los datos para cada día y dispositivo. Se deben cambiar el nombre de la tabla dependiendo del día para el que se necesiten los datos
+    # Se debe cambiar la variable res.mac a la dirección del dispositivo para el que se desee generar la gráfica
+    # El retorno consiste en una lista de tuplas que contienen la mac, tiempo y número de observadores
+    postgreSQL_select_Query = "SELECT * FROM(SELECT json_array_elements(data->'data'->'observations')->>'clientMac' as mac,json_array_elements(data->'data'->'observations')->>'seenTime' as time,json_array_length(json_array_elements(data->'data'->'observations')->'deviceObservers') as observers FROM public.example15dic ORDER BY mac,time) as res WHERE res.mac='F81F32423A55';"
 
     cursor.execute(postgreSQL_select_Query)
     print("Doing select time, mac and observers")
     mobile_records = cursor.fetchall()
 
+# Carga de los datos en un dataFrame del framework pandas
     df = pd.DataFrame(data=mobile_records, columns=[
                       'mac', 'time', 'observers'])
 
+# Cambio de formato de la variable del tiempo. Se pasa de string a datetime
     df['time'] = pd.to_datetime(df['time'])
     df.index = df['time']
+# Agrupación de las visitas por tiempo para que la gráfica salga ordenada por minutos
     df_g = df.groupby(pd.Grouper(freq='1Min')).aggregate(np.sum)
     print(df_g)
-
+# Ploteo de la gráfica en formato de barras
     df_g.plot.bar()
     plt.tight_layout()
-
-    # fig, ax = plt.subplots(figsize=(20, 10))
-    # ax.bar(df_g.index, df_g['observers'])
     plt.show()
 
 
 finally:
-    # closing database connection.
+    # Cierre de la conexión con la base de datos
     if(connection):
         cursor.close()
         connection.close()
--- VP_2Dic/vectorProximity.py
+++ VP_2Dic/vectorProximity.py
@@ -10,8 +10,13 @@ from itertools import groupby
 from operator import itemgetter
 import os
 
-AVERAGE_POTENCE = 70
+# El siguiente script tiene como fin la definición y ejecución del algoritmo de vectores de proximidad definido en el documento de grado.
+# Además, tiene los datos necesarios para la ejecución e impresión de pruebas para el día 2 de diciembre
+
+# Valor minimo considerado para el RSSI, parámetro usado por el método de VP
 LOW_RSSI = -100
+
+# Clase de perfil de señal. Contiene la mac, tiempo y la lista de aps con sus rssi vistos por un dispositivo
 
 
 class signal_profile_object:
@@ -21,6 +26,8 @@ class signal_profile_object:
         self.aps = []
         self.rssis = []
 
+# Clase de perfil de señal continuo. Contiene la mac, rangos de tiempo y los aps con sus rssi vistos por un dispositivo en dicho rango de tiempo.
+
 
 class continuous_profile_object:
     def __init__(self, mac, start_time, final_time, aps, rssis):
@@ -29,6 +36,8 @@ class continuous_profile_object:
         self.final_time = final_time
         self.aps = aps
         self.rssis = rssis
+
+# Metodo que carga el resultado del query a la base de datos a la clase de perfil de señal
 
 
 def signal_profile(records):
@@ -45,15 +54,20 @@ def signal_profile(records):
         profile.append(po)
     return profile
 
+# Metodo que crea un perfil de señal continuo a partir de la clase perfil de señal
+
 
 def continuous_signal(signal_profile_input):
     signal_profile = signal_profile_input.copy()
     obj = signal_profile[len(signal_profile)-1]
+    # Creación del último rango de tiempo.
     signal_profile.append(signal_profile_object(obj.mac, obj.time+60000))
     continuous_profile = []
     for i in range(len(signal_profile)-1):
+        # Unión de los conjuntos de APs
         join_aps = list(set(signal_profile[i].aps + signal_profile[i+1].aps))
         join_rssis = []
+        # Definición de los rangos validos de rssi para la unión de conjuntos de APs
         for ap in join_aps:
             try:
                 index1 = signal_profile[i].aps.index(ap)
@@ -76,12 +90,17 @@ def continuous_signal(signal_profile_input):
             signal_profile[i].mac, signal_profile[i].time, signal_profile[i+1].time, join_aps, join_rssis))
     return continuous_profile
 
+# Método que calcula el valor de proximidad P a partir de un objeto de perfil de señal y un objeto de perfil de señal continuo(infectado)
+
 
 def signal_proximity(continuous_object, signal_object):
+    # Cantidad de dispositivos en común
     overlap = len([w for w in signal_object.aps if w in continuous_object.aps])
+    # Valor O
     overlap_ratio = overlap / \
         min(len(continuous_object.aps), len(signal_object.aps))
     dif = 0
+    # Calculo del valor D
     for i in range(len(signal_object.aps)):
         difference = 0
         try:
@@ -100,7 +119,10 @@ def signal_proximity(continuous_object, signal_object):
     if(overlap == 0):
         return 0
     avg_difference = dif/overlap
+    # Valor P
     return overlap_ratio/(avg_difference+1)
+
+# Método que calcula el valor de proximidad para cada instante de tiempo que compartan dos perfiles de señal(uno continuo)
 
 
 def signal_metrics(infected_continuous, subject_profile):
@@ -124,6 +146,8 @@ def signal_metrics(infected_continuous, subject_profile):
     file1.close()
     return metrics
 
+# Definición de errores y excepciones de la conexión a base de datos
+
 
 def print_psycopg2_exception(err):
     # get details about the exception
@@ -139,6 +163,7 @@ def print_psycopg2_exception(err):
     print("pgcode:", err.pgcode, "\n")
 
 
+# Instantes de tiempo para cada prueba realizada el día 2 de diciembre
 tests = [
     ('Prueba 2', 1606938960000, 1606939380000),
     ('Prueba 4', 1606939380000, 1606939740000),
@@ -159,6 +184,8 @@ tests = [
     ('Prueba 9_3', 1606945380000, 1606945680000),
     ('Prueba 10_3', 1606945680000, 1606946040000),
 ]
+
+# Calculo de los valores P, desviación estandar y IOD para cada prueba
 
 
 def testAverages(metrics, suspect, infected):
@@ -190,40 +217,36 @@ def testAverages(metrics, suspect, infected):
 
 connection = None
 try:
+    # Conexión a la base de datos
     connection = psycopg2.connect(
         user="postgres", password="postgres", host="127.0.0.1", port="5432", database="pdgdata")
     cursor = connection.cursor()
-    # postgreSQL_select_Query = "SELECT * FROM(SELECT json_array_elements(data->'data'->'observations')->>'clientMac' as mac,json_array_elements(data->'data'->'observations')->>'seenEpoch' as time,(json_array_elements(data->'data'->'observations')->'deviceObservers') as observers FROM public.example ORDER BY mac,time) as res;"
-    # cursor.execute(postgreSQL_select_Query)
-    # allData = cursor.fetchall()
-    # seq = allData
-    # #seq.sort(key = lambda x: x[0])
-    # groups = groupby(seq, lambda x: x[0])
-    # new_list = [[item[:] for item in data] for (key, data) in groups]
-    # for element in new_list:
-    #     suceptible_profile = signal_profile(element)
-    #     metrics = signal_metrics(infected_continuous, suceptible_profile)
-    #     print('Analisis para: '+str(element[0][0]))
-    #     print(metrics)
+    # Lista de direcciones MAC con las que se realizaron las pruebas
     macs_list = ['F81F32F89FB4', 'F81F32F8A61E', 'F81F32F8A5D4']
+    # Definición de salida en Excel en caso de querer comparar cada par de dispositivos
     # writer = pd.ExcelWriter('output.xlsx', engine='xlsxwriter')
     df = pd.DataFrame()
+    # Ciclos para realizar las pruebas de todos los elementos de la lista de MAC con todos los otros
     for mac_suspect in macs_list:
         for mac_infected in macs_list:
             if mac_suspect != mac_infected:
+                # Consulta para traer los datos del perfil continuo(infectado)
                 postgreSQL_select_Query = "SELECT * FROM(SELECT json_array_elements(data->'data'->'observations')->>'clientMac' as mac,json_array_elements(data->'data'->'observations')->>'seenEpoch' as time,(json_array_elements(data->'data'->'observations')->'deviceObservers') as observers FROM public.example2dic ORDER BY mac,time) as res WHERE res.mac='"+mac_infected+"';"
                 cursor.execute(postgreSQL_select_Query)
                 records = cursor.fetchall()
                 infected_profile = signal_profile(records)
                 infected_continuous = continuous_signal(infected_profile)
+                # Consulta para traer los datos del perfil a comparar
                 postgreSQL_select_Query = "SELECT * FROM(SELECT json_array_elements(data->'data'->'observations')->>'clientMac' as mac,json_array_elements(data->'data'->'observations')->>'seenEpoch' as time,(json_array_elements(data->'data'->'observations')->'deviceObservers') as observers FROM public.example2dic ORDER BY mac,time) as res WHERE res.mac='"+mac_suspect+"';"
                 cursor.execute(postgreSQL_select_Query)
                 records = cursor.fetchall()
                 suspect_profile = signal_profile(records)
+                # Calculo de metricas de resultados
                 metrics = signal_metrics(infected_continuous, suspect_profile)
                 print('Analisis para: '+mac_infected+' con: '+mac_suspect)
                 series = testAverages(metrics, mac_suspect, mac_infected)
                 df = df.append(series, ignore_index=True)
+                # Guardar el resultado de cada par de pruebas en una tabla de excel
                 # create DataFrame using data
                 # df = pd.DataFrame(metrics, columns=[
                 #                   'Proximity', 'StartT', 'EndT', 'MeasuredT'])
@@ -233,7 +256,7 @@ try:
     print(df)
 
 finally:
-    # closing database connection.
+    # Cerrar conexión a base de datos
     if(connection):
         cursor.close()
         connection.close()
